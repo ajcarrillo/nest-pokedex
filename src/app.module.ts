f500c0eea02741840542889e2cede3722623a532
Andrés Carrillo Herrera <andresjch2804@gmail.com>
2022-07-25 00:08:52 -0500

    Section 7 part 69

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from "@nestjs/common";
+import { join } from "path";
+import { ServeStaticModule } from "@nestjs/serve-static";
 
 @Module({
-  imports: []
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "public")
+    })
+  ]
 })
 export class AppModule {
 }
