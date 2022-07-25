2a6a35e4c274c781d928496ec423c8d029a529dd
Andrés Carrillo Herrera <andresjch2804@gmail.com>
2022-07-25 01:21:21 -0500

    Section 7 part 77

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,11 +1,28 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, InternalServerErrorException } from "@nestjs/common";
 import { CreatePokemonDto } from "./dto/create-pokemon.dto";
 import { UpdatePokemonDto } from "./dto/update-pokemon.dto";
+import { Pokemon } from "./entities/pokemon.entity";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return "This action adds a new pokemon";
+  constructor(
+    @InjectModel(Pokemon.name) private pokemonModel: Model<Pokemon>
+  ) {
+  }
+
+  async create(createPokemonDto: CreatePokemonDto) {
+    try {
+      createPokemonDto.name = createPokemonDto.name.toLowerCase();
+      return await this.pokemonModel.create(createPokemonDto);
+    } catch (e) {
+      if (e.code === 11000) {
+        throw new BadRequestException(`Pokemon already exists ${JSON.stringify(e.keyValue)}`);
+      }
+      console.log(e);
+      throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`);
+    }
   }
 
   findAll() {
