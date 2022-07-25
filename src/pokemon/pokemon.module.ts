195378e6e0d526936e464aca0886dfa76707d5df
Andrés Carrillo Herrera <andresjch2804@gmail.com>
2022-07-25 02:44:19 -0500

    Section 8 part 93

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -11,7 +11,8 @@ import PokemonSchema, { Pokemon } from "./entities/pokemon.entity";
     MongooseModule.forFeature([
       { name: Pokemon.name, schema: PokemonSchema }
     ])
-  ]
+  ],
+  exports: [MongooseModule]
 })
 export class PokemonModule {
 }
