f22ccffa49c42cd76aeb50ab1b50e617f3226078
Andrés Carrillo Herrera <andresjch2804@gmail.com>
2022-07-25 00:15:24 -0500

    Section 7 part 70

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from "@nestjs/common";
 import { join } from "path";
 import { ServeStaticModule } from "@nestjs/serve-static";
+import { PokemonModule } from "./pokemon/pokemon.module";
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, "..", "public")
-    })
+    }),
+    PokemonModule
   ]
 })
 export class AppModule {
