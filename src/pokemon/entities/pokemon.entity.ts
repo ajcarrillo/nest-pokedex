949b6d14d68c63f11e734b06838e9bf5c08fef3d
Andrés Carrillo Herrera <andresjch2804@gmail.com>
2022-07-25 00:46:02 -0500

    Section 7 part 74

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,2 +1,31 @@
-export class Pokemon {
+import { Document } from "mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class Pokemon extends Document {
+  @Prop({
+    required: true,
+    unique: true,
+    index: true
+  })
+  name: string;
+
+  @Prop({
+    required: true,
+    unique: true,
+    index: true
+  })
+  no: number;
 }
+
+const PokemonSchema = SchemaFactory.createForClass(Pokemon);
+
+PokemonSchema.set("toJSON", {
+  transform: (doc, ret) => {
+    ret.id = ret._id.toString();
+    delete ret._id;
+    delete ret.__v;
+  }
+});
+
+export default PokemonSchema;
